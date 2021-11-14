22b5957828b3ed57f3f5ffe0e914b911aabc6aae
Ahmed Mimo <91825697+5217193@users.noreply.github.com>
2021-11-14 18:40:21 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -2,5 +2,20 @@
 using namespace std;
 
 int main() {
+   int i, n, a=1, b=1, c=0;
+   
+   cin>>n;
+   
+   if (n>1) {
+      cout<<a<<endl;
+      cout<<b<<endl;
+   } else {
+      cout<<"error"<<endl;
+   }
+   for(i=1;i<n;i++) {
+      c=a+b;
+      b=c
+         cout<<c<endl;
+   }
    return 0;
 }
